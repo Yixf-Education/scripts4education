6d9031d0310d5fcc70891b2db98a4c22628346a7
Yixf(Yi Xianfu) <yixfgit@gmail.com>
2016-06-02 15:59:11 +0800

    Rewrite gpra.pl for questions2paper

--- questions2paper/gpra.pl
+++ questions2paper/gpra.pl
@@ -66,11 +66,11 @@ my @exclude      = &parser_clude( $config{exclude} );
 my %distribution = &parser_distribution();
 
 #Step4: Do some checks
-&check_type();
-&check_question_number();
-&check_point();
-&check_chapter();
-&check_score();
+&check_config_score();
+&check_config_type();
+&check_config_chapter();
+&check_config_point();
+&check_config_type_chapter();
 
 #Step5: Get the library
 #Only needed files are stored into %lib
@@ -81,19 +81,22 @@ foreach my $lib_file (@lib_files) {
     my ( $filename, $dirs, $suffix ) = fileparse( $lib_file, ".tex" );
     my ( $ab, $type, $chapter, $seq ) = split /_/, $filename;
     if ( lc($ab) eq $paper_ab ) {
-        $lib_count_ab{ lc($ab) }++;
-        if ( List::Util::any { $_ eq $type } @types ) {
-            $lib_count_type{ lc($ab) }->{$type}++;
-            if ( List::Util::any { $_ eq $chapter } @chapters ) {
+        if ( exists $distribution{$type} ) {
+            if ( exists $distribution{$type}->{$chapter} ) {
+                $lib_count_ab{ lc($ab) }++;
+                $lib_count_type{ lc($ab) }->{$type}++;
                 $lib_count_chapter{ lc($ab) }->{$type}->{$chapter}++;
                 $lib{$filename} = $lib_file;
             }
         }
+        else {
+            if ( List::Util::any { $_ eq $type } @types ) {
+                $lib_count_ab{ lc($ab) }++;
+                $lib_count_type{ lc($ab) }->{$type}++;
+                $lib{$filename} = $lib_file;
+            }
+        }
     }
-}
-
-unless (%lib) {
-    die "For 'a_or_b': There is no library for '$paper_ab', exit!\n";
 }
 
 #Clect warnings in modify_lib
@@ -101,9 +104,10 @@ my @warnings;
 my %exclude_fullname = &modify_lib("exclude");
 
 #Do some more checks
-#Check for enough questions in the library, this check must is between '&modify_lib("exclude")' and '&modify_lib("include")'
+#Check for the existence and enough questions in the library, this check must is between '&modify_lib("exclude")' and '&modify_lib("include")'
 #This check exclude excluded questions
-&check_type_chapter;
+&check_lib_exist();
+&check_lib_enough();
 my %include_fullname = &modify_lib("include");
 
 #Step6: Shuffle the questions in the library to make them random
@@ -273,18 +277,35 @@ sub parser_clude {
 #Parse the question distribution in chapters for each question type
 sub parser_distribution {
     my %hash;
+    my $counter = 0;
     foreach my $type (@types) {
         foreach my $chapter ( sort keys $config{$type} ) {
             if ( $chapter =~ /^\d+$/ ) {
+                $counter++;
                 $hash{$type}->{$chapter} = $config{$type}->{$chapter};
             }
+        }
+        unless ( $counter > 0 ) {
+            delete $hash{$type};
         }
     }
     return %hash;
 }
 
+#Check total score of the test paper, which should be equal to the sum of points for each question type (zongfen.testPaper=sum(zongfen.types))
+sub check_config_score {
+    my $sum;
+    foreach my $type (@types) {
+        $sum += $config{$type}->{zongfen};
+    }
+    unless ( $sum == $config{zongfen} ) {
+        die
+"For score: The sum of points for each question type does not equal to the total score of the test paper.\n";
+    }
+}
+
 #Check the question types (tixing, 题型)
-sub check_type {
+sub check_config_type {
     foreach my $type (@types) {
         unless ( exists $config{$type} ) {
             die
@@ -293,8 +314,34 @@ sub check_type {
     }
 }
 
+#Check chapters (zhangjie, 章节)
+sub check_config_chapter {
+    foreach my $type (@types) {
+        foreach my $key ( sort keys $config{$type} ) {
+            if ( $key =~ /^\d+$/ ) {
+                unless ( List::Util::any { $_ == $key } @chapters ) {
+                    die
+"For '$type': The chapter '$key' does not exist in '$config{zhangjie}' chapters.\n";
+                }
+            }
+        }
+    }
+}
+
+#Check points (zongfen=fen*tishu, 总分=小题分*题目数) for each question type
+sub check_config_point {
+    foreach my $type (@types) {
+        unless ( $config{$type}->{zongfen} ==
+            $config{$type}->{fen} * $config{$type}->{tishu} )
+        {
+            die
+"For '$type': The result, point for each question times question number, does not equal to total points.\n";
+        }
+    }
+}
+
 #Check the question number (xiaotishu, 小题数) for each question type
-sub check_question_number {
+sub check_config_type_chapter {
     foreach my $type (@types) {
         my $sum;
         foreach my $key ( sort keys $config{$type} ) {
@@ -309,71 +356,6 @@ sub check_question_number {
             }
         }
     }
-}
-
-#Check points (zongfen=fen*tishu, 总分=小题分*题目数) for each question type
-sub check_point {
-    foreach my $type (@types) {
-        unless ( $config{$type}->{zongfen} ==
-            $config{$type}->{fen} * $config{$type}->{tishu} )
-        {
-            die
-"For '$type': The result, point for each question times question number, does not equal to total points.\n";
-        }
-    }
-}
-
-#Check chapters (zhangjie, 章节)
-sub check_chapter {
-    foreach my $type (@types) {
-        foreach my $key ( sort keys $config{$type} ) {
-            if ( $key =~ /^\d+$/ ) {
-                unless ( List::Util::any { $_ == $key } @chapters ) {
-                    die
-"For '$type': The chapter '$key' does not exist in '$config{zhangjie}' chapters.\n";
-                }
-            }
-        }
-    }
-}
-
-#Check total score of the test paper, which should be equal to the sum of points for each question type (zongfen.testPaper=sum(zongfen.types))
-sub check_score {
-    my $sum;
-    foreach my $type (@types) {
-        $sum += $config{$type}->{zongfen};
-    }
-    unless ( $sum == $config{zongfen} ) {
-        die
-"For score: The sum of points for each question type does not equal to the total score of the test paper.\n";
-    }
-}
-
-#Generate the TeX configure file (TIJMUexam.cls)
-sub generate_tex_cfg {
-    my $O_file_name = $tex_cfg_file;
-    open my $O, '>', $O_file_name
-      or die "$0 : failed to open output file '$O_file_name' : $!\n";
-    select $O;
-    print "\\ProvidesFile{$tex_cfg_file}\n\n";
-    print "\\def\\\@xuexiao{$config{xuexiao}}\n";
-    print "\\def\\\@yearb{$config{yearb}}\n";
-    print "\\def\\\@yeare{$config{yeare}}\n";
-    print "\\def\\\@xueqi{$config{xueqi}}\n";
-    print "\\def\\\@zhuanye{$config{zhuanye}}\n";
-    print "\\def\\\@kecheng{$config{kecheng}}\n";
-    print "\\def\\\@abjuan{$config{abjuan}}\n";
-    print "\\def\\\@kaibijuan{$config{kaibijuan}}\n\n";
-    print "\\def\\\@zongfen{$config{zongfen}}\n";
-    print "\\def\\\@shijian{$config{shijian}}\n\n";
-
-    foreach my $type (@types) {
-        print "\\def\\\@$type\@zongfen{$config{$type}->{zongfen}}\n";
-        print "\\def\\\@$type\@tishu{$config{$type}->{tishu}}\n";
-        print "\\def\\\@$type\@fen{$config{$type}->{fen}}\n\n";
-    }
-    print "\\endinput";
-    close $O or warn "$0 : failed to close output file '$O_file_name' : $!\n";
 }
 
 #Exclude the items configured in 'include' and 'exclude', and get the full path for each question
@@ -411,27 +393,52 @@ sub modify_lib {
     return %hash;
 }
 
-sub check_type_chapter {
-    my ( %number_type, %number_chapter, %number_type_chapter );
+sub check_lib_exist {
+    unless ( exists $lib_count_ab{$paper_ab} ) {
+        die "There is no library for paper type '$paper_ab', exit!\n";
+    }
+    foreach my $type (@types) {
+        unless ( exists $lib_count_type{$paper_ab}->{$type} ) {
+            die
+"There is no library for question type '$type' of paper type '$paper_ab', exit!\n";
+        }
+        if ( exists $lib_count_type{$paper_ab}->{$type} ) {
+            if ( exists $distribution{$type} ) {
+                foreach my $chapter ( sort keys $distribution{$type} ) {
+                    unless (
+                        exists $lib_count_chapter{$paper_ab}->{$type}
+                        ->{$chapter} )
+                    {
+                        die
+"There is no library for 'chapter $chapter' of question type '$type' of paper type '$paper_ab', exit!\n";
+                    }
+                }
+            }
+        }
+    }
+}
+
+sub check_lib_enough {
+    my ( %number_type, %number_type_chapter );
     foreach my $filename ( sort keys %lib ) {
         my ( $ab, $type, $chapter, $seq ) = split /_/, $filename;
-        $number_chapter{$chapter}++;
-        $number_type{$type}++;
-        $number_type_chapter{$type}->{$chapter}++;
-    }
-    foreach my $chapter (@chapters) {
-        unless ( exists $number_chapter{$chapter} ) {
-            die
-"For chapter: The 'chapter $chapter' does not exist in the library!\n";
+
+#Important: This line is needed, otherwise new key(s) will be added to %distribution
+        if ( exists $distribution{$type} ) {
+            if ( exists $distribution{$type}->{$chapter} ) {
+                $number_type_chapter{$type}->{$chapter}++;
+            }
+        }
+        else {
+            $number_type{$type}++;
         }
     }
     foreach my $type (@types) {
-        unless ( $number_type{$type} >= $config{$type}->{tishu} ) {
-            die
-"For '$type': There is not enough questions in the library, own($number_type{$type}) < need($config{$type}->{tishu})!\n";
-        }
         if ( exists $distribution{$type} ) {
             foreach my $chapter ( sort keys $distribution{$type} ) {
+                unless ( exists $number_type_chapter{$type}->{$chapter} ) {
+                    $number_type_chapter{$type}->{$chapter} = 0;
+                }
                 unless ( $number_type_chapter{$type}->{$chapter} >=
                     $distribution{$type}->{$chapter} )
                 {
@@ -440,28 +447,70 @@ sub check_type_chapter {
                 }
             }
         }
+        else {
+            unless ( $number_type{$type} >= $config{$type}->{tishu} ) {
+                die
+"For '$type': There is not enough questions in the library, own($number_type{$type}) < need($config{$type}->{tishu})!\n";
+            }
+        }
     }
+}
+
+#Generate the TeX configure file (TIJMUexam.cls)
+sub generate_tex_cfg {
+    my $O_file_name = $tex_cfg_file;
+    open my $O, '>', $O_file_name
+      or die "$0 : failed to open output file '$O_file_name' : $!\n";
+    select $O;
+    print "\\ProvidesFile{$tex_cfg_file}\n\n";
+    print "\\def\\\@xuexiao{$config{xuexiao}}\n";
+    print "\\def\\\@yearb{$config{yearb}}\n";
+    print "\\def\\\@yeare{$config{yeare}}\n";
+    print "\\def\\\@xueqi{$config{xueqi}}\n";
+    print "\\def\\\@zhuanye{$config{zhuanye}}\n";
+    print "\\def\\\@kecheng{$config{kecheng}}\n";
+    print "\\def\\\@abjuan{$config{abjuan}}\n";
+    print "\\def\\\@kaibijuan{$config{kaibijuan}}\n\n";
+    print "\\def\\\@zongfen{$config{zongfen}}\n";
+    print "\\def\\\@shijian{$config{shijian}}\n\n";
+
+    foreach my $type (@types) {
+        print "\\def\\\@$type\@zongfen{$config{$type}->{zongfen}}\n";
+        print "\\def\\\@$type\@tishu{$config{$type}->{tishu}}\n";
+        print "\\def\\\@$type\@fen{$config{$type}->{fen}}\n\n";
+    }
+    print "\\endinput";
+    close $O or warn "$0 : failed to close output file '$O_file_name' : $!\n";
 }
 
 #Generate TeX snippets for each question type
 sub generate_snippet {
     my ( $type, $question_ref ) = @_;
-    my $O_file_name = File::Spec->catfile( "$snippet_dir",
+    my $O_file_type = File::Spec->catfile( "$snippet_dir",
         "$paper_ab" . "_" . "$type" . ".tex" );
     if ($backup) {
-        &rotate_file($O_file_name);
+        &rotate_file($O_file_type);
     }
-    open my $O, '>', $O_file_name
-      or die "$0 : failed to open output file '$O_file_name' : $!\n";
+    open my $O, '>', $O_file_type
+      or die "$0 : failed to open output file '$O_file_type' : $!\n";
     select $O;
     print "\\" . "$type\n\n";
     foreach my $question (@$question_ref) {
         print "\\input{$question}\n";
     }
     if ( $type =~ /xuan/ ) {
-        &draw_choice_table($type);
+        my $O_file_table = File::Spec->catfile( "$snippet_dir",
+            "$paper_ab" . "_" . "$type" . "_table" . ".tex" );
+        if ($backup) {
+            &rotate_file($O_file_table);
+        }
+        open my $OT, '>', $O_file_table
+          or die "$0 : failed to open  output file '$O_file_table' : $!\n";
+        &draw_choice_table( $type, $OT );
+        close $OT
+          or warn "$0 : failed to close output file '$O_file_table' : $!\n";
     }
-    close $O or warn "$0 : failed to close output file '$O_file_name' : $!\n";
+    close $O or warn "$0 : failed to close output file '$O_file_type' : $!\n";
 }
 
 #Backup old files
@@ -475,7 +524,7 @@ sub rotate_file {
 
 #Draw the answer table for the question type choice - single or multiple (xuanze - danxuan & duoxuan)
 sub draw_choice_table {
-    my $type = shift(@_);
+    my ( $type, $fh ) = @_;
     my $step;
     if ( $type eq "danxuan" ) {
         $step = 10;
@@ -483,30 +532,47 @@ sub draw_choice_table {
     if ( $type eq "duoxuan" ) {
         $step = 5;
     }
-    print "\n\\begin{tabularx}{0.92\\textwidth}{|c|";
-    print "Y|" x $step;
-    print "}\n";
-    print "\\hline\n";
-    print "题号";
-    my $count = $config{$type}->{tishu};
-    my $flag  = $count / $step;
+    print $fh "\n\\begin{tabularx}{0.92\\textwidth}{|c|";
+    print $fh "Y|" x $step;
+    print $fh "}\n";
+    print $fh "\\hline\n";
+    my $count  = $config{$type}->{tishu};
+    my $flag   = $count / $step;
+    my $offset = 0;
 
-    for ( my $i = 0 ; $i < $flag ; $i++ ) {
-        my $start = $step * $i + 1;
-        my $end = $step * ( $i + 1 );
-        foreach my $i ( $start .. $end ) {
-            print " & $i";
+    foreach my $tp (@types) {
+        if ( $tp ne $type ) {
+            $offset += $config{$tp}->{tishu};
         }
-        print "\\" x 2;
-        print "\n";
-        print "\\hline\n";
-        print "答案";
-        print " &" x $step;
-        print "\\" x 2;
-        print "\n";
-        print "\\hline\n";
+        else {
+            last;
+        }
     }
-    print "\\end{tabularx}\n";
+
+    my $counter;
+    for ( my $i = 0 ; $i < $flag ; $i++ ) {
+        print $fh "题号";
+        my $start = $offset + $step * $i + 1;
+        my $end = $offset + $step * ( $i + 1 );
+        foreach my $i ( $start .. $end ) {
+            $counter++;
+            if ( $counter <= $config{$type}->{tishu} ) {
+                print $fh " & $i";
+            }
+            else {
+                print $fh " & ---";
+            }
+        }
+        print $fh "\\" x 2;
+        print $fh "\n";
+        print $fh "\\hline\n";
+        print $fh "答案";
+        print $fh " &" x $step;
+        print $fh "\\" x 2;
+        print $fh "\n";
+        print $fh "\\hline\n";
+    }
+    print $fh "\\end{tabularx}\n";
 }
 
 #Generate the final TeX files (with and without answers) for the test paper
@@ -527,17 +593,34 @@ sub generate_paper {
     #0 for without answers, 1 for with answers
     &paper_head( $OP,  0 );
     &paper_head( $OPA, 1 );
-    my @items;
+    my ( @items, %tables );
     foreach my $type (@types) {
         push @items, <$snippet_dir/$paper_ab\_$type.tex>;
-    }
-    foreach my $item (@items) {
-        my $bn = basename($item);
-        if ( $bn =~ /^$paper_ab/ ) {
-            &paper_body( $OP,  $item );
-            &paper_body( $OPA, $item );
+        if ( $type =~ /xuan/ ) {
+            $tables{$type} = "$snippet_dir/$paper_ab\_$type\_table.tex";
         }
     }
+    print $OP "\\begin{questions}\n\n";
+    print $OPA "\\begin{questions}\n\n";
+    foreach my $item (@items) {
+
+        #if ( $bn =~ /^$paper_ab/ ) {
+        &paper_body( $OP,  $item );
+        &paper_body( $OPA, $item );
+
+        #Add choice table for paper without answers
+        my $bn = basename($item);
+        $bn =~ s/\.tex//;
+        my ( $ab, $type ) = split /_/, $bn;
+        if ( $type =~ /xuan/ ) {
+            print $OP "\\input{$tables{$type}}\n";
+            print $OP "\n";
+        }
+
+        #}
+    }
+    print $OP "\\end{questions}\n\n";
+    print $OPA "\\end{questions}\n\n";
     &paper_tail($OP);
     &paper_tail($OPA);
     close $OP or warn "$0 : failed to close output file '$fo_paper' : $!\n";
@@ -572,15 +655,19 @@ sub paper_head {
 #Write the body (each question type) for final TeX file
 sub paper_body {
     my ( $fh, $question ) = @_;
-    print $fh "\\begin{questions}\n";
+
+    #print $fh "\\begin{questions}\n";
     print $fh "\\input{$question}\n";
-    print $fh "\\end{questions}\n";
+
+    #print $fh "\\end{questions}\n";
     print $fh "\n";
 }
 
 #Write the tail for final TeX file
 sub paper_tail {
     my ($fh) = shift(@_);
+    print $fh "\\vspace{0.8\\textheight plus 0.2\\textheight minus 0.8\\textheight}\n";
+    print $fh "\n";
     print $fh "\\end{framed}\n";
     print $fh "\n";
     print $fh "\\end{document}\n";
@@ -708,6 +795,7 @@ The TeX file name for final test paper.
 =head1 VERSION
  
 V1 (20141214)
+V2 (20141215)
 
 =head1 AUTHOR
 
